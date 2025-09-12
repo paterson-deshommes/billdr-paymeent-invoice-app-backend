6061236905d31ff0412a3f12c9d35ca575a2a12f
Paterson Deshommes <pat14_14@hotmail.com>
2025-09-12 01:01:08 -0400

    fetch secret key and allowed host from environment variables

--- PaymentInvoiceBackend/settings.py
+++ PaymentInvoiceBackend/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-3%qu-^hl6%=u%cefjbdnndgz2+t7u0u36$*gl7%*vpt%(%$unn"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOST", "localhost").split(",")
 
 
 # Application definition
